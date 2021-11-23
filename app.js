5a8ccf6dc3f3b39db2e24bd54f7d11123a6414bf
PabloValles <valles.pablo@hotmail.com>
2021-11-23 16:28:38 -0300

    cambiamos objetos a array

--- app.js
+++ app.js
@@ -1,45 +1,38 @@
 const carrito = document.querySelector("#carrito");
 const template = document.querySelector("#template");
-// Necesitamos el fragment para evitar el reflow
 const fragment = document.createDocumentFragment();
 const btns = document.querySelectorAll(".card .btn");
 
-// Creamos nuestro carrito
-const carritoObjeto = {};
+//const carritoObjeto = {};
+const carritoObjeto = [];
 
-// Creamos las funciones
 // agregar al carrito
 const agregarAlCarrito = (e) => {
-  // Capturamos la fruta del data-fruta del btn
-  //console.log(e.target.dataset.fruta);
-
-  // Creamos nuestro objeto producto
   const producto = {
     titulo: e.target.dataset.fruta,
     id: e.target.dataset.fruta,
     cantidad: 1,
   };
 
-  // Tenemos que preguntarle al objeto si el producto ya existe, y si es así sumarle uno a la cantidad
-  if (carritoObjeto.hasOwnProperty(producto.id)) {
-    producto.cantidad = carritoObjeto[producto.id].cantidad + 1;
+  const indice = carritoObjeto.findIndex((item) => item.id === producto.id);
+  console.log(indice);
+
+  if (indice === -1) {
+    carritoObjeto.push(producto);
+  } else {
+    // Si el elemento existe trae el elemento y a la cantidad le sumamnos uno
+    carritoObjeto[indice].cantidad++;
   }
-  // Le agregamos al Carrito nuestro obj. producto
-  // carritoObjeto[aqui va a ir el nombre del objeto]
-  carritoObjeto[producto.titulo] = producto;
 
-  // Pintamos nuestro producto para
-  pintarCarrito(producto);
+  console.log(carritoObjeto);
 
-  //console.log(carritoObjeto);
+  pintarCarrito(carritoObjeto);
 };
 
-const pintarCarrito = (producto) => {
+const pintarCarrito = (array) => {
   carrito.textContent = "";
 
-  // Con Objet.values podemos recorrer el objeto como si fuera un array
-  Object.values(carritoObjeto).forEach((item) => {
-    //console.log(producto);
+  array.forEach((item) => {
     const clon = template.content.firstElementChild.cloneNode(true);
     clon.querySelector(".lead").textContent = item.titulo;
     clon.querySelector(".badge").textContent = item.cantidad;
